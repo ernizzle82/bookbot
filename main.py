f8119d139e8713562835e06cff7f24e72bd171d5
Ernie_Velazquez <ernie.velazquez@live.com>
2025-08-16 16:26:30 -0700

    renamed functions to match bootdev, added chars_dict_to_sorted_list function, added report printing

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-from stats import count_characters, count_words
-
+from stats import get_num_words, chars_dict_to_sorted_list, get_chars_dict
 
 def get_book_text(path_to_file: str) -> str:
     with open(path_to_file, "r", encoding="utf-8") as file:
@@ -10,10 +9,23 @@ def get_book_text(path_to_file: str) -> str:
 
 def main():
     path_to_file = "books/frankenstein.txt"
+
     file_contents = get_book_text(path_to_file)
 
-    print(f"{count_words(file_contents)} words found in the document")
-    print(count_characters(file_contents))
+    character_counts = get_chars_dict(file_contents)
+
+    sorted_dicts = chars_dict_to_sorted_list(character_counts)
+
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {path_to_file}...")
+    print("----------- Word Count ----------")
+    print(f"Found {get_num_words(file_contents)} total words")
+    print("--------- Character Count -------")
+    for item in sorted_dicts:
+        if not item["char"].isalpha():
+            continue
+        print(f"{item['char']}: {item['num']}")
+    print("============= END ===============")
 
 
 if __name__ == "__main__":
--- stats.py
+++ stats.py
@@ -1,9 +1,9 @@
-def count_words(text: str) -> int:
+def get_num_words(text: str) -> int:
     word_list = text.split()
     return len(word_list)
 
 
-def count_characters(text: str) -> dict:
+def get_chars_dict(text: str) -> dict:
     # create dictionary to store letter counts
     char_count = dict()
 
@@ -15,3 +15,21 @@ def count_characters(text: str) -> dict:
             char_count[char] = 1
 
     return char_count
+
+
+def sort_on_num_key(items: dict) -> int:
+    return items["num"]
+
+def chars_dict_to_sorted_list(dictionary: dict) -> list:
+    # create empty list to hold dictionarie pairs
+    list_of_dicts = list()
+
+    # populate list of dicts
+    for key in dictionary:
+        current_dict = {"char": key, "num": dictionary[key]}
+        list_of_dicts.append(current_dict)
+
+    # sort list of dicts
+    list_of_dicts.sort(key=sort_on_num_key, reverse=True)
+    return list_of_dicts
+
